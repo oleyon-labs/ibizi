2b9720fd632b1fdeef9f135ab765562539789288
oley <kruuula@mail.ru>
2022-09-10 14:10:08 +0300

    Добавьте файлы проекта.

---
+++ ibizi/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string data = "шифрование_перестановкой_";
+var k1 = new byte[] { 0, 1, 2, 3, 4 };
+var k2 = new byte[] { 0, 1, 2, 3, 4 };
+var encryptedData = Lab1.TranspositionCipher.Encrypt(data, k1, k2);
+var decryptedData = Lab1.TranspositionCipher.Encrypt(encryptedData, k1, k2);
+Console.WriteLine($"data: {data}");
+Console.WriteLine($"encrypted data: {encryptedData}");
+Console.WriteLine($"decrypted data: {decryptedData}");
---
+++ ibizi/TranspositionCipher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class TranspositionCipher
+    {
+        public static string Encrypt(string data, byte[] horizontalKey, byte[] verticalKey)
+        {
+            if (data.Length > horizontalKey.Length * verticalKey.Length)
+                throw new Exception("key is too short");
+            /*for (int i = 0; i < horizontalKey.Length; i++)
+                horizontalKey[i] -= 1;
+            for(int i = 0; i < verticalKey.Length; i++)
+                verticalKey[i] -= 1;*/
+            StringBuilder encryptedData = new StringBuilder(data);
+            for (int i = 0; i < encryptedData.Length; i++)
+            {
+                encryptedData[verticalKey[i % verticalKey.Length] * verticalKey.Length + horizontalKey[i / horizontalKey.Length]] = data[i];
+            }
+            return encryptedData.ToString();
+        }
+    }
+}
